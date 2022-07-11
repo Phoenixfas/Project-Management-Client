fa60f1cf6195f7b0ba0988bac412e7b583ba418f
Fasika Dagnachew <fasika1515@gmail.com>
2022-07-11 22:24:10 +0300

    Project card a tag to Link

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export default function ProjectCard({project}) {
   return (
     <div className="col-md-4">
@@ -5,7 +7,7 @@ export default function ProjectCard({project}) {
             <div className="card-body">
                 <div className="d-flex justify-content-between align-items-center">
                     <h5 className="card-title">{project.name}</h5>
-                    <a href={`/projects/${project.id}`} className="btn btn-light">View</a>
+                    <Link to={`/projects/${project.id}`} className="btn btn-light">View</Link>
                 </div>
                 <p className="small">
                     Status: <strong>{project.status}</strong>
