f5eacf3d71b8f52df15a1a3bf4b3a0843012b18b
Fasika Dagnachew <fasika1515@gmail.com>
2022-07-11 16:21:25 +0300

    added correct api(url) for apollo client server

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: "https://project-management-mern.herokuapp.com/graphql",
   cache,
 });
 
